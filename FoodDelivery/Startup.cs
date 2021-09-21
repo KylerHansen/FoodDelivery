995c92c378d57f599e91d49910d824bbd6836985
KylerHansen <kylerhansen@mail.weber.edu>
2021-09-21 17:03:30 -0600

    Added Application User and Roles

--- FoodDelivery/Startup.cs
+++ FoodDelivery/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
 using ApplicationCore.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Infrastructure.Services;
 
 namespace FoodDelivery
 {
@@ -41,6 +43,38 @@ namespace FoodDelivery
             services.AddScoped<IUnitofWork, UnitofWork>();
 
             services.AddMvc(options => options.EnableEndpointRouting = false); //disabled end point routing see 61-64 is commented out.
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<AuthSenderOptions>(Configuration);
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,6 +95,8 @@ namespace FoodDelivery
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();  //order matters.
 
             app.UseAuthorization();
 
